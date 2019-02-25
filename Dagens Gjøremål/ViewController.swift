e5fc040dceb8f943811eac3e20713f28df12ded2
Rhs <Samnoy@lcv-rhss-mac-mini.local>
2019-02-25 14:59:20 +0100

    Initial Commit

---
+++ Dagens Gjøremål/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dagens Gjøremål
+//
+//  Created by Rhs on 25/02/2019.
+//  Copyright © 2019 Lcv-Rhs. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
